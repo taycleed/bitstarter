41ea05c9282e36824a606a204b2c17a4873d0cff
Namho Kim <taycleed@gmail.com>
2013-07-12 05:39:14 +0000

    HW3 Part3 completed.

--- grader.js
+++ grader.js
@@ -24,14 +24,17 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "http://www.google.com";
 
+// http://nodejs.org/api/process.html#process_process_exit_code
 var assertFileExists = function(infile) {
     var instr = infile.toString();
     if(!fs.existsSync(instr)) {
         console.log("%s does not exist. Exiting.", instr);
-        process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	process.exit(1); 
     }
     return instr;
 };
@@ -55,6 +58,24 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksfile){
+    rest.get(url).on('complete', function(result){
+	if(result instanceof Error){
+       	    console.log('Error: ' + result.message);
+            this.retry(5000);
+        } else {
+	    $ = cheerio.load(result);
+	    var checks = loadChecks(checksfile).sort();
+	    var out = {};
+	    for(var ii in checks) {
+		var present = $(checks[ii]).length > 0;
+		out[checks[ii]] = present;
+	    }
+	    console.log(JSON.stringify(out, null, 4));
+        }
+    });
+};
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -64,11 +85,20 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists)) //, HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'Path to url')//, URL_DEFAULT)
+	.parse(process.argv);
+
+	if(program.file != null){
+		var checkJson = checkHtmlFile(program.file, program.checks);
+    		var outJson = JSON.stringify(checkJson, null, 4);
+		console.log(outJson);
+	}else if(program.url != null){
+		checkUrl(program.url, program.checks);
+	}else{
+		program.help();	
+	}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
+
